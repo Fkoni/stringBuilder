d99d7d14c8e1c3367e97823cac6f632e7cfca1e3
Fkoni <93380100+Fkoni@users.noreply.github.com>
2022-01-07 09:30:44 +0300

    Add files via upload

---
+++ C05_Reverse.java
@@ -0,0 +1,24 @@
+package day33_stringBuilder;
+
+public class C05_Reverse {
+
+	public static void main(String[] args) {
+		// verilen bir cümleyi tersten yazdıralım
+		
+		StringBuilder sb = new StringBuilder("Java ne kadar kolay");
+		
+		sb.reverse();
+		
+		System.out.println(sb);
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
